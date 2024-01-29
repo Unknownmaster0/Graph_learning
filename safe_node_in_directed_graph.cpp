bd6d67ddd92bde95bf44e771877c999f22b25572
Unknownmaster0 <sagar192002singh@gmail.com>
2024-01-29 22:36:46 +0530

    new approach using the topological sort

--- safe_node_in_directed_graph.cpp
+++ safe_node_in_directed_graph.cpp
@@ -1,8 +1,61 @@
 #include<iostream>
 #include<vector>
+#include<queue>
+#include<algorithm>
 using namespace std;
 
 // problem link -> https://www.geeksforgeeks.org/problems/eventual-safe-states/1
+
+/* solving this question is more efficient from the DFS algo of cycle detection method , but you should also know the topo sort approach.
+
+    Why the topo sort approach is not efficient ?
+     -> Because, it is more time taking than DFS one. [DFS time = O(v + e) in worst case] [topo sort = O(v + e) + O(v * log(v)) in worst case.]
+*/
+
+/*Time complexity -> O(v + e) + O(v * log(v)) [extra one for sorting the safeNodes vector]
+    Space complexity -> O(v) + O(v) [something more than dfs algo.]
+*/
+// using topological sort algorithm 
+    vector<int> eventualSafeNodes(int v, vector<int> adj[])
+    {
+        // first make the reverse graph or adjacent list
+        vector<int> adjrev[v];
+        int indegree[v] = {0};
+        
+        for(int i = 0; i<v; i++)
+        {
+            for(auto it:adj[i]){
+                
+                adjrev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+        
+        // now simple apply the topo sort
+        queue<int> q;
+        for(int i = 0; i<v; i++)
+            if(indegree[i] == 0)    q.push(i);
+        
+        vector<int> safeNodes;
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            
+            safeNodes.push_back(node);
+            
+            for(auto it:adjrev[node]){
+                indegree[it]--;
+                if(indegree[it] == 0)q.push(it);
+            }
+        }
+        
+        sort(safeNodes.begin(), safeNodes.end());
+        
+        return safeNodes;
+        
+    }
 
     bool dfs(int node, vector<int>adj[], vector<int>& vis, vector<int>& pathvis, int check[]){
         vis[node] = 1;
